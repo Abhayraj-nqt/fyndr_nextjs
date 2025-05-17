739e939c5816dace73e72be2b30ef7fd5f3f4d7f
Abhayrajdunerya-nqt <abhayraj.dunerya@nucleusteq.com>
2025-05-17 12:40:36 +0530

    upgraded eslint

--- next.config.ts
+++ next.config.ts
@@ -2,55 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pino", "pino-pretty"],
+  // eslint: { ignoreDuringBuilds: true },
+  // typescript: { ignoreBuildErrors: true },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "s3.us-west-1.amazonaws.com",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "s3-us-west-1.amazonaws.com",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "png.pngtree.com",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "github.com",
-        port: "",
-      },
+      { protocol: "https", hostname: "s3.us-west-1.amazonaws.com", port: "" },
+      { protocol: "https", hostname: "s3-us-west-1.amazonaws.com", port: "" },
+      { protocol: "https", hostname: "png.pngtree.com", port: "" },
+      { protocol: "https", hostname: "github.com", port: "" },
     ],
   },
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
-  // cacheMaxMemorySize: 4 * 1024 * 1024, // 4MB
-
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
-
-  // typescript: {
-  //   // !! WARN !!
-  //   // Dangerously allow production builds to successfully complete even if
-  //   // your project has type errors.
-  //   // !! WARN !!
-  //   ignoreBuildErrors: true,
-  // },
-
-  // experimental: {
-  //   staleTimes: {
-  //     dynamic: 30,
-  //     static: 180,
-  //   },
-  // },
+  logging: { fetches: { fullUrl: true } },
 };
 
 export default nextConfig;
