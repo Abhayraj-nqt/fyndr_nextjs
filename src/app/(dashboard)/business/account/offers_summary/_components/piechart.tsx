c0af67902f3c279028cd8b5e8277c25a80e8f687
animeshbarole02 <animesh.barole@nucleusteq.com>
2025-05-20 07:58:56 +0530

    Added Offer Summary API with pie chart

---
+++ src/app/(dashboard)/business/account/offers_summary/_components/piechart.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import { PieChart, Pie, Tooltip, Cell } from "recharts";
+
+interface ChartData {
+  name: string;
+  visitors: number;
+}
+
+interface PieChartProps {
+  chartData: ChartData[];
+  colors?: string[];
+}
+
+const PieChartSection: React.FC<PieChartProps> = ({ chartData, colors = ["#5196E2", "#999999", "#EAF2FC"] }) => {
+  return (
+
+      <PieChart width={250} height={250}>
+        <Pie
+          data={chartData}
+          dataKey="visitors"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={75}
+          label
+        >
+          {chartData.map((_, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+      </PieChart>
+   
+  );
+};
+
+export default PieChartSection;
