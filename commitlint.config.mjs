72df6ac93973decc11b310f1f03703655786b031
Abhayrajdunerya-nqt <abhayraj.dunerya@nucleusteq.com>
2025-05-28 23:03:21 +0530

    feat: integrated husky with commitlint

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,17 +1,3 @@
-// module.exports = {
-//   extends: ["@commitlint/config-conventional"],
-//   rules: {
-//     "type-enum": [
-//       2,
-//       "always",
-//       ["feat", "fix", "wip", "patch", "build", "test", "revert"],
-//     ],
-//     "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
-//     "subject-max-length": [2, "always", 72],
-//     "body-max-line-length": [2, "always", 100],
-//   },
-// };
-
 export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
