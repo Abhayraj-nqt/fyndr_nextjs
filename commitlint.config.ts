451c67e53350dee417d52c04a11257ea762b5f22
Abhayrajdunerya-nqt <abhayraj.dunerya@nucleusteq.com>
2025-05-28 22:23:04 +0530

    added commit lint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,6 +1,13 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "wip", "patch", "build"]],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "wip", "patch", "build", "test", "revert"],
+    ],
+    "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
+    "subject-max-length": [2, "always", 72],
+    "body-max-line-length": [2, "always", 100],
   },
 };
